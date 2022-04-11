2a7a70147dfd9376543129bb89d7e76f3cada535
ramazantufekci <ramazan.tufekci@outlook.com>
2022-04-11 07:43:58 +0300

    sign up form

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './component/Navbar';
+import Signin from './pages/Auth/Signin';
+import Signup from './pages/Auth/Signup';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar/>
+        <div id="content">
+			<Switch>
+			  <Route path="/" exact component={Products}/>
+			  <Route path="/product/:product_id" component={ProductDetail}/>
+			  <Route path="/signin" component={Signin}/>
+			  <Route path="/signup" component={Signup}/>			  
+			</Switch>
+		</div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/ProductDetail/index.js
@@ -0,0 +1,34 @@
+import {useQuery} from 'react-query';
+import { useParams } from 'react-router-dom';
+import { Box, Text, Button} from '@chakra-ui/react';
+import { fetchProduct } from '../../api';
+import moment from 'moment';
+import ImageGallery from 'react-image-gallery';
+
+function ProductDetail(){
+	const {product_id} = useParams();
+	const {isLoading, isError, data} = useQuery(["product",product_id], ()=> fetchProduct(product_id));
+	if(isLoading){
+		return <div> Loading...</div>
+	}
+	
+	if(isError){
+		return <div> Error.</div>
+	}
+	
+	const images = data.photos.map((url)=>({original:url}))
+	return (<div>
+	
+		<Button colorScheme="pink">Add to basket</Button>
+		<Text as="h2" fontSize="2x1">
+		{data.title}
+		</Text>
+		<Text>{moment(data.createdAt).format("DD/MM/YYYY")}</Text>
+		<p>{data.description}</p>
+		<Box margin="10">
+		<ImageGallery items={images}/>
+		</Box>
+	</div>);
+}
+
+export default ProductDetail;
