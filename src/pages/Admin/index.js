14319fe61c63c59ce5945155d43d9e49f92ae7f8
ramazantufekci <ramazan.tufekci@outlook.com>
2022-04-12 11:14:10 +0300

    product update

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -5,6 +5,7 @@ import { Link, Switch, Route, useRouteMatch } from 'react-router-dom';
 import Home from './Home';
 import Orders from './Orders';
 import Products from './Products';
+import ProductDetail from './ProductDetail';
 
 function Admin(){
 	const {path,url} = useRouteMatch();
@@ -27,7 +28,8 @@ function Admin(){
 			<Switch>
 				<Route exact path={path} component={Home}/>
 				<Route path={`${path}/orders`} component={Orders}/>
-				<Route path={`${path}/products`} component={Products}/>
+				<Route exact path={`${path}/products`} component={Products}/>
+				<Route path={`${path}/products/:product_id`} component={ProductDetail}/>
 			</Switch>
 		</Box>
 	</div>);
